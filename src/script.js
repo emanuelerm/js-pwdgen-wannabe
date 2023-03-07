dd16e6899c520c5ff33ccc34aef557d8826833ec
emanuelerm <emanueler00@gmail.com>
2023-03-07 15:58:43 +0100

    isNaN and else added

--- src/script.js
+++ src/script.js
@@ -4,7 +4,9 @@ prompt; {
     let cognomeUtente = prompt('Inserisci il tuo cognome');
     let colorePreferito = prompt('Inserisci il tuo colore preferito');
 
-    if (nomeUtente && cognomeUtente && colorePreferito) {
+    if ((isNaN (nomeUtente)) && (isNaN (cognomeUtente)) && (isNaN (colorePreferito))) {
         document.getElementById('insicurissimaPw').innerHTML = nomeUtente + cognomeUtente + colorePreferito + number
+    } else {
+        document.getElementById('insicurissimaPw').innerHTML = 'Inserire i dati correttamente'
     }
 }
